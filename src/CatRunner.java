ec6bbddf7f0d5b4dbac431f93f34bc7f1045f4eb
student <sample@bths.edu>
2022-09-23 12:25:22 -0400

    Please work!!!

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class CatRunner {
+public class CatRunner
+{
+    public static void main(String[]args)
+    {
+        Cat cat1 = new Cat("Staten Island", 3, 10);
+        cat1.introduce();
+        cat1.printCatInfo();
+
+        Cat cat2 = new Cat("Manhattan", 7, 5);
+        cat2.introduce();
+        cat2.printCatInfo();
+    }
 }
